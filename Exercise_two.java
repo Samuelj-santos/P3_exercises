5fafa135e458942e1ce588f41b3bf7de2e372c2c
Samuelj-santos <samuel.00000847429@unicap.br>
2023-08-14 15:36:19 +0000

    Update

---
+++ Exercise_two.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Exercise_two {
+ static Scanner s = new Scanner (System.in);
+ public static void main(String[] args) {
+    
+ }
+
+    public String  Question01(){
+ //1. Solicite ao usuário seu nome e imprima uma mensagem de boas-vindas na tela.
+
+    String nome = s.next();
+    System.out.println("Bem Vindo" + nome);
+    }
+}
---
+++ teste.java
@@ -0,0 +1,5 @@
+import java.util.Scanner;
+public class teste {
+    Scanner s = new Scanner (System.in);
+    
+}

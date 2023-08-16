167bbb185ff6831fa81d73bc6f3e9e4eeed2ab17
Samuelj-santos <samuel.00000847429@unicap.br>
2023-08-15 21:33:22 -0300

    Conclude

--- Exercise_three.java
+++ Exercise_three.java
@@ -3,45 +3,45 @@ import java.util.Scanner;
 public class Exercise_three {
  static Scanner s = new Scanner (System.in);
  public static void main(String[] args) {
-   // Question01();
+  //  Question01();
   // Question02();
   //Question03();
   //Question04();
- // Question05();
+  //Question05();
   //Question06();
   //Question07();
-  //Question08();
-  //Question09();
-  //Question10();
- // Question11();
- // Question12();
+ // Question08();
+ // Question09();
+ // Question10();
+  //Question11();
+  //Question12();
   //Question13();
   //Question14();
-  //Question15();
+ // Question15();
   //Question16();
-  //Question17();
-  //Question18();
-  //Question19();
-  //Question20();
-  //Question21();
-  //Question22();
+  Question17();
+  Question18();
+  Question19();
+  Question20();
+  Question21();
+  Question22();
   Question23();  
-  //Question24();
-  //Question25();
-  //Question26();
-  //Question27();
-  //Question28();
-  //Question29();
-  //Question30();
-  //Question31();
-  //Question32();
-  //Question33();
-  //Question34();
-  //Question35();
-  //Question36();
-  //Question37();
-  //Question38();
-  //Question39();
+  Question24();
+  Question25();
+  Question26();
+  Question27();
+  Question28();
+  Question29();
+  Question30();
+  Question31();
+  Question32();
+  Question33();
+  Question34();
+  Question35();
+  Question36();
+  Question37();
+  Question38();
+  Question39();
 
   
  }
@@ -89,6 +89,7 @@ System.out.println("Insira seu nome");
     }
     public static void Question06(){
       //6. Peça ao usuário que insira o seu ano de nascimento e, em seguida, exiba a sua idade.
+      System.out.println("Insira seu ano de nascimento");
       int nascimento = s.nextInt();
       int idade = 2023 - nascimento;
       System.out.println("Sua idade é " +idade);
@@ -105,6 +106,21 @@ System.out.println("Insira seu nome");
     public static void Question08(){
       //8. Solicite ao usuário uma sequência de números separados por espaço e exiba quantos números
       //foram digitados.
+      System.out.println("Digite uma sequencai de numeros");
+      String a  = s.nextLine();
+      int cont =0;
+      if (a.length() != 0) {
+        cont ++;
+        
+      }
+      for(int i = 0;i<a.length();i++){
+       char c = a.charAt(i);
+       if (Character.isWhitespace(c)) {
+        cont++;
+        
+       }
+      }
+      System.out.println("O numero de digitos que voce digitou foi " + cont);
       
     }
     public static void Question09(){
@@ -121,7 +137,7 @@ System.out.println("Insira seu nome");
       String nome = s.next();
       System.out.println("Insira seu sobrenome");
      String sobrenome = s.next();
-     System.out.println("Seu sobrenome é "+ sobrenome + "E seu nome é" + nome);
+     System.out.println("Seu sobrenome é "+ sobrenome + " E seu nome é " + nome);
     }
     public static void Question11(){
       //11. Receba uma string digitada pelo usuário e imprima o seu tamanho (número de caracteres).
@@ -178,6 +194,7 @@ System.out.println("Insira seu nome");
       double b = s.nextDouble();
       double imc = (b)/(a *a);
       System.out.printf( " Seu IMC é " + "%.1f" , imc);
+      System.out.println();
     }
     public static void Question16(){
       //16. Peça ao usuário que digite o seu nome e verifique se ele contém mais de 5 caracteres.
@@ -197,8 +214,10 @@ System.out.println("Insira seu nome");
     public static void Question17(){
       //16. Peça ao usuário que digite o seu nome e verifique se ele contém mais de 5 caracteres.
       System.out.println("Insira seu estado civil");
-      String a = s.next();
-      if (a == "solteiro") {
+      String a = s.nextLine();
+      String sol= "solteiro";
+      String casado = "casado";
+      if (a.equals(sol) ) {
         System.out.println("Você é solteiro(a)");
         
       }else{
@@ -233,7 +252,7 @@ System.out.println("Insira seu nome");
       float a = s.nextFloat();
       System.out.println("Insira o segundo numero decimal");
       float b = s.nextFloat();
-      System.out.println("o resto da divisão é " + a%b);
+      System.out.println("o resto da divisão é " + (a%b));
     }
     public static void Question21(){
       //21. Solicite ao usuário um número decimal e converta-o em um número inteiro.
@@ -261,69 +280,151 @@ System.out.println("Insira seu nome");
       char[] b = a.toCharArray();
       System.out.println("o dia é " + b[0]+b[1]);
       System.out.println("O mes é " + b[3]+b[4]);
-      System.out.println("O ano é " + b[6]+b[7]);
+      System.out.println("O ano é " + b[6]+b[7]+b[8]+b[9]);
     }
     public static void Question24(){
       //24. Receba o nome de uma cidade do usuário e concatene-o com o nome do estado para formar uma
       //mensagem completa, como "Você mora em [cidade], [estado].".
+      System.out.println("Insira sua cidade");
+      String a = s.next();
+      System.out.println("Insira o estado em que você reside");
+      String b = s.next();
+      System.out.println("Você mora em " + a + ", " + b);
     }
     public static void Question25(){
       //25. Solicite ao usuário que insira seu ano de nascimento e concatene-o com uma mensagem de
       //boas-vindas, como "Bem-vindo ao nosso programa, nascido em [ano de nascimento]!".
+      System.out.println("Insira seu ano de nascimento");
+      String a = s.next();
+      System.out.println("Bem vindo ao nosso programa nascido em " + a);
     }
     public static void Question26(){
       //26. Receba um número inteiro e uma string do usuário. Em seguida, concatene-os em uma única
       //string, separando-os com um espaço.
+      System.out.println("Insira um numero inteiro");
+      int a = s.nextInt();
+      System.out.println("Insira uma String");
+      String b = s.next();
+      String c = Integer.toString(a);
+      System.out.println(c + " " + b);
     }
     public static void Question27(){
       //27. Receba o nome de um produto digitado pelo usuário e concatene-o com o preço do produto,
       //adicionando o símbolo de moeda da sua escolha.
+      System.out.println("Insira o nome do seu produto");
+      String a = s.next();
+      double  preço = 20.50;
+      System.out.println("Seu produto "+a + " custa " + "R$:" + preço);
     }
     public static void Question28(){
       //28. Receba um número inteiro do usuário e concatene-o com uma mensagem, informando o dobro
       //desse número.
+      System.out.println("Insira um numero");
+      double a = s.nextDouble();
+      System.out.printf("O dobro desse numero é "+ "%.1f",a*2);
+      System.out.println();
+
     }
     public static void Question29(){
       //29. Receba uma string contendo um endereço de e-mail e concatene-a com uma mensagem de
       //agradecimento personalizada.
+      System.out.println("Insira seu email");
+      String a = s.next();
+      System.out.println("Muito Obrigado pelo feedback " + a);
     }
     public static void Question30(){
       //30. Receba dois números inteiros do usuário e exiba a soma, a diferença, o produto e o quociente    //(divisão inteira entre eles.
+      System.out.println("Insira um numero inteiro");
+      int a = s.nextInt();
+      System.out.println("Insira seu segundo numero");
+      int b = s.nextInt();
+      System.out.println("A soma desses numeros  é "+ (a+b) );
+      System.out.println("A diferença entre esses numeros é " + (a-b));
+      System.out.println("O produto entre esses numeros é " + (a*b));
+      System.out.println("O quociente entre esses numeros é " + (a/b));
+
 
     }
     public static void Question31(){
       //31. Peça ao usuário para digitar a base e a altura de um triângulo. Em seguida, calcule e exiba a área do
       //triângulo.
+      System.out.println("Insira a base do  triangulo ");
+      double a = s.nextDouble();
+      System.out.println("Insira a altura do triangulo ");
+      double b  = s.nextDouble();
+      System.out.println("A área do  triangulo é " + ((a*b)/2));
+
     }
     public static void Question32(){
       //32. Receba o raio de uma circunferência digitado pelo usuário e calcule o seu perímetro (2 * π * raio).
+    System.out.println("Insira o raio da circuferencia ");
+     double a = s.nextDouble();
+     System.out.println("O perimetro da circuferencia é " + (2*3.14*a));
+
     }
     public static void Question33(){
       //33. Receba a base e a altura de um retângulo digitados pelo usuário. Em seguida, calcule e exiba o
       //perímetro do retângulo.
+      System.out.println("Insira a base do retangulo");
+      double a = s.nextDouble();
+      System.out.println("Insira a altura do retangulo");
+      double b = s.nextDouble();
+      System.out.println("O perimetro do retangulo é " + (2*(a+b)));
     }
     public static void Question34(){
       //34. Solicite ao usuário que insira três números decimais. Em seguida, calcule e exiba a média
       //aritmética desses números.
+      System.out.println("Insira o primeiro numero");
+      double a = s.nextDouble();
+      System.out.println("Insira o segundo numero");
+      double b = s.nextDouble();
+      System.out.println("Insira o terceiro numero");
+      double c = s.nextDouble();
+      System.out.println("A media desses numeros é " + ((a+b+c)/3));
     }
     public static void Question35(){
       //35. Peça ao usuário para digitar a sua idade e, em seguida, informe quantos meses e quantos dias ele já
       //viveu (considerando um ano com 365 dias).
+      System.out.println("Digite a sua idade");
+      int a = s.nextInt();
+      System.out.println("Voce ja viveu " + (365*a));
     }
     public static void Question36(){
       //36. Receba um valor em reais e a cotação do dólar digitados pelo usuário. Em seguida, converta o valor
       //para dólares e exiba o resultado.
+      System.out.println("Insira um valor em rais");
+      double a = s.nextDouble();
+      System.out.println("Insira a cotaçao do dolar ");
+      double b = s.nextDouble();
+      System.out.println("O valor convertido é " +"US$" +  (a/b));
+
     }
     public static void Question37(){
       //37. Solicite ao usuário para digitar um número decimal e arredonde-o para o inteiro mais próximo.
+      System.out.println("Insira um numero decimal");
+      double a = s.nextDouble();
+      int b = (int) a;
+      System.out.println("O inteiro mais proximo é " + b);
+
     }
     public static void Question38(){
       //38. Receba três números inteiros digitados pelo usuário e exiba o resultado da operação (n1 + n2) *
       //n3.
+      System.out.println("Insira o primeiro numero inteiro");
+      int a = s.nextInt();
+      System.out.println("Insira o segundo numero");
+      int b = s.nextInt();
+      System.out.println("Insira o terceiro numero");
+      int c = s.nextInt();
+      System.out.println("O valor de (n1+n2)*n3 é" + ((a+b)*c));
+
     }
     public static void Question39(){
       //39. Peça ao usuário que digite uma temperatura em graus Celsius e a converta para Fahrenheit usando
       //a fórmula: Fahrenheit = (Celsius * 9/5) + 32.
+      System.out.println("Insira uma temperatura em celsius");
+      double a = s.nextDouble();
+      System.out.println("Sua temperatura em fahrenheit é " + ((a*(9/5))+32));
     }
 
 

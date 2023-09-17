a15ba4351bb495ff69c0d16ee25c968aa1492815
Samuelj-santos <samuel.00000847429@unicap.br>
2023-09-17 17:32:05 -0300

    Update

---
+++ Exercise_Nine/Question_01/Funcionario.java
@@ -0,0 +1,45 @@
+package Exercise_Nine.Question_01;
+
+public class Funcionario {
+    private String nome;
+    private double salarioAnual;
+    private int idade;
+
+
+    public Funcionario(String nome, double salarioAnual, int idade) {
+        this.nome = nome;
+        this.salarioAnual = salarioAnual;
+        this.idade = idade;
+    }
+    
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getSalarioAnual() {
+        return salarioAnual;
+    }
+
+    public void setSalarioAnual(double salarioAnual) {
+        this.salarioAnual = salarioAnual;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    @Override
+    public String toString(){
+        return this.nome + " e de salario " + this.salarioAnual;
+    }
+
+    
+}
---
+++ Exercise_Nine/Question_01/Main.java
@@ -0,0 +1,25 @@
+package Exercise_Nine.Question_01;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList <Funcionario> Funcionarios = new ArrayList<Funcionario>();
+        Funcionario alan = new Funcionario("Alan",15.000 , 30);
+        Funcionario nataly = new Funcionario("Nataly", 17.000, 25);
+        Funcionario arthur = new Funcionario("Arthur", 35.000, 29);
+        Funcionario nicolas = new Funcionario("Nicolas", 32.000, 24);
+        Funcionario rodrigo = new Funcionario("Rodrigo", 37.000, 26);
+        Funcionarios.add(rodrigo);
+        Funcionarios.add(nicolas);
+        Funcionarios.add(arthur);
+        Funcionarios.add(nataly);
+        Funcionarios.add(alan);
+
+        for(int i =0;i<Funcionarios.size();i++){
+           if ( Funcionarios.get(i).getSalarioAnual() >= 30.000 && Funcionarios.get(i).getIdade() <= 30 ) {
+            System.out.println("O funcionario " + Funcionarios.get(i).toString());
+           }
+        }
+    }
+}

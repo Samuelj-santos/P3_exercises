ad1e732518f1c70b73754a9aa531a36163e7877a
Samuelj-santos <samuel.00000847429@unicap.br>
2023-10-22 18:01:37 -0300

    Exercise twelve quase pronto

---
+++ Exercise_Twelve/Biblioteca/Livro.java
@@ -0,0 +1,10 @@
+package Exercise_Twelve.Biblioteca;
+
+public class Livro  extends ItemBiblioteca{
+
+    public Livro(String titulo, int anodePublicaçao, int copiasDisponiveis) {
+        super(titulo, anodePublicaçao, copiasDisponiveis);
+        
+    }
+    
+}
